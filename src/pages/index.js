04e8d7cadea528aaad90b8ba6334b30f161ad071
alireza <fanaticboy666@gmail.com>
2019-04-14 08:11:17 +0430

    refactoring index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,55 +8,50 @@ import Container from 'components/Container';
 import {rhythm} from '../lib/typography';
 import theme from '../../config/theme';
 
-const PostTitle = styled.h2`margin-bottom: ${rhythm (0.3)}; transition: ${theme.transition.ease}; :hover {color : $ {theme.brand.primary};
+const PostTitle = styled.h2 `margin-bottom: ${rhythm(0.3)}; transition: ${theme.transition.ease}; :hover {color : $ {theme.brand.primary};
   transition : $ {theme.transition.ease};
 }`;
-const Description = styled.p` margin-bottom: 10px; display: inline-block; `;
-export default function Index({data: {site, allMdx}}) {
+const Description = styled.p ` margin-bottom: 10px; display: inline-block; `;
+export default function Index({
+  data: {
+    site,
+    allMdx
+  }
+}) {
   return (
     <Layout
       site={site}
       headerColor={theme.colors.white}
-      headerBg={theme.brand.primary}
-    >
-      <Container css={css` padding-bottom: 0; `}>
-        {allMdx.edges.map (({node: post}) => (
-          <div
-            key={post.id}
-            css={css` margin-bottom: 40px; direction: rtl; text-align: right; `}
-          >
-            <Link
-              to={post.frontmatter.slug}
-              aria-label={`View ${post.frontmatter.title}`}
-            >
-              <PostTitle>{post.frontmatter.title}</PostTitle>
-            </Link>
-            <Description>
-              {post.excerpt}{' '}
-              <Link
-                to={post.frontmatter.slug}
-                aria-label={`View ${post.frontmatter.title}`}
-              >
-                متن کامل ←
+      headerBg={theme.brand.primary}>
+      <Container css={css ` padding-bottom: 0; `}>
+        {allMdx
+          .edges
+          .map(({node: post}) => (
+            <div
+              key={post.id}
+              css={css ` margin-bottom: 40px; direction: rtl; text-align: right; `}>
+              <Link to={post.frontmatter.slug} aria-label={`View ${post.frontmatter.title}`}>
+                <PostTitle>{post.frontmatter.title}</PostTitle>
               </Link>
-            </Description>
-            <span />
-          </div>
-        ))}
-        <Link
-          to="/blog"
-          aria-label="Visit blog page"
-          className="button-secondary"
-        >
+              <Description>
+                {post.excerpt}{' '}
+                <Link to={post.frontmatter.slug} aria-label={`View ${post.frontmatter.title}`}>
+                  متن کامل ←
+                </Link>
+              </Description>
+              <span/>
+            </div>
+          ))}
+        <Link to="/blog" aria-label="Visit blog page" className="button-secondary">
           دیدن همه‌ی مطلب‌ها
         </Link>
-        <hr />
+        <hr/>
       </Container>
     </Layout>
   );
 }
 
-export const pageQuery = graphql`query {site {
+export const pageQuery = graphql `query {site {
     ...site siteMetadata {
       title
     }
@@ -85,8 +80,8 @@ export const pageQuery = graphql`query {site {
         frontmatter {
           title date(formatString : "MMMM DD, YYYY") description banner {
             childImageSharp {
-              sizes(maxWidth : 720) {
-                ...GatsbyImageSharpSizes
+              fluid(maxWidth: 720,maxHeight: 530, traceSVG: {color: "#de4d44"}) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
               }
             }
           }
